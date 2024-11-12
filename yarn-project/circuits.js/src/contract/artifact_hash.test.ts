814019bd8aa0eb7d9fafbd146668873fd91a46d3
sklppy88 <esau@aztecprotocol.com>
2024-11-12 15:47:21 +0000

    init

--- yarn-project/circuits.js/src/contract/artifact_hash.test.ts
+++ yarn-project/circuits.js/src/contract/artifact_hash.test.ts
@@ -1,5 +1,6 @@
 import { type ContractArtifact } from '@aztec/foundation/abi';
 
+import { getTestContractArtifact } from '../tests/fixtures.js';
 import { computeArtifactHash } from './artifact_hash.js';
 
 describe('ArtifactHash', () => {
@@ -18,5 +19,15 @@ describe('ArtifactHash', () => {
     expect(computeArtifactHash(emptyArtifact).toString()).toMatchInlineSnapshot(
       `"0x0c6fd9b48570721c5d36f978d084d77cacbfd2814f1344985f40e62bea6e61be"`,
     );
+  });
+
+  it('calculates the test contract artifact hash multiple times to ensure deterministic hashing', () => {
+    const testArtifact = getTestContractArtifact();
+
+    for (let i = 0; i < 1000; i++) {
+      expect(computeArtifactHash(testArtifact).toString()).toMatchInlineSnapshot(
+        `"0x16b7b028aaaa06d648cc521c385644d7786d2f1ae49157b27a424b172b298162"`,
+      );
+    }
   });
 });
